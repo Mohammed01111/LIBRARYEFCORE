760035cc2a1e07a6c76ca774afc019f24d7ae861
Mohammed <Mohammed.almanwari.01@gmail.com>
2024-11-13 11:28:37 +0400

    Add project files.

---
+++ LIBRARYEFCORE/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using LIBRARYEFCORE.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LIBRARYEFCORE
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Borrowing> Borrowings { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(" Data Source=DESKTOP-VFQRVL8\\MSSQLSERVER02; Initial Catalog=LIBRARYEFCORE; Integrated Security=true; TrustServerCertificate=True ");
+        }
+
+    }   
+
+}
---
+++ LIBRARYEFCORE/Repositories/UserRepository.cs
@@ -0,0 +1,62 @@
+﻿using LIBRARYEFCORE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIBRARYEFCORE.Repositories
+{
+    public class UserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+
+        public User GetByName(string name)
+        {
+            return _context.Users.FirstOrDefault(u => u.UName == name);
+        }
+
+        public void Insert(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+
+        public void UpdateByName(string name, User updatedUser)
+        {
+            var user = GetByName(name);
+            if (user != null)
+            {
+                user.UName = updatedUser.UName;
+                user.Gender = updatedUser.Gender;
+                user.Passcode = updatedUser.Passcode;
+                _context.SaveChanges();
+            }
+        }
+
+        public void DeleteById(int id)
+        {
+            var user = _context.Users.Find(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+            }
+        }
+
+        public int CountByGender(string gender)
+        {
+            return _context.Users.Count(u => u.Gender == gender);
+        }
+    }
+}
